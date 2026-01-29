a43354a69e698a4831053eac008231016fbf55ed
KAS1NG <kageburado99@gmail.com>
2026-01-29 09:21:52 +0200

    Update project3

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -12,7 +12,7 @@ export const categories = [
     { name: 'Столи', mainUrl: '/products/stoly', href: '/products/stoly?page=1&query=стіл' },
     { name: 'Матраци', mainUrl: '/products/matrats', href: '/products/matrats?page=1&query=матрац' },
     { name: 'Тумби', mainUrl: '/products/tumbs', href: '/products/tumbs?page=1&query=тумба' },
-    { name: 'Комодики', mainUrl: '/products/tumbs', href: '/products/komods?page=1&query=комод' },
+    { name: 'Комоди', mainUrl: '/products/komods', href: '/products/komods?page=1&query=комод' },
 ];
 
 export const businessSchema = {
